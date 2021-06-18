2a0d5a80e5b938667c7edb6cfdcc6d8f5188b995
DudeBatista <maria.santos1894@etec.sp.gov.br>
2021-06-18 18:39:35 -0300

    Add project files.

---
+++ ExCadastro/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExCadastro.Models
+{
+    public class Produto
+    {
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Valor inválido!")]
+        public string Peso { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório!")]
+        public string Categoria { get; set; }
+
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Valor inválido!")]
+        public string valorUnico { get; set; }
+    }
+}
